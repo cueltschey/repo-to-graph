ab7f86161e5bec8a988eba71340fe76d51062c9c
Charles Ueltschey <chaseuelt@gmail.com>
2024-07-12 21:59:39 -0500

    created a cool tool

--- main.py
+++ main.py
@@ -4,26 +4,8 @@ import mimetypes
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Initialize a directed graph
 
-# Add nodes (you can dynamically add more nodes later)
-G.add_node("Node 1", color="blue")
-G.add_node("Node 2", color="green")
-G.add_node("Node 3", color="red")
 
-# Add edges between nodes
-G.add_edge("Node 1", "Node 2")
-G.add_edge("Node 2", "Node 3")
-G.add_edge("Node 3", "Node 1")
-
-# Get node colors for visualization
-node_colors = [G.nodes[node]["color"] for node in G.nodes]
-
-# Draw the graph with matplotlib
-pos = nx.spring_layout(G)  # positions for all nodes
-nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=1000)
-nx.draw_networkx_edges(G, pos, arrows=True)
-nx.draw_networkx_labels(G, pos, font_size=12, font_family="sans-serif")
 
 # Display the graph
 
@@ -34,23 +16,56 @@ def is_text_file(filepath):
 
 
 def walk_directory(path, tabs=""):
+    global G
+    global module_set
     print(f"{tabs}entering directory: ", path)
     for file in os.listdir(path):
         if(os.path.isdir(os.path.join(path, file))):
             walk_directory(os.path.join(path, file), tabs+"\t")
         elif is_text_file(os.path.join(path, file)):
-            print(f"{tabs}-" + file)
+            print(f"{tabs}-" + os.path.join(path, file))
+            file_module = '/'.join(os.path.join(path, file).split('/')[1:])
+            if not file_module in module_set:
+                G.add_node(file_module, color='green')
+                module_set.add(file_module)
             try:
                 with open(os.path.join(path, file), 'r') as f:
-                    print(f.readlines())
+                    for line in f.readlines()[:50]:
+                        if '#include \"' in line:
+                            module_name = line.split('\"')[1]
+                            if not module_set in module_set:
+                                G.add_node(module_name, color="green")
+                                module_set.add(module_name)
+                            G.add_edge(module_name, file_module)
+
+
             except UnicodeDecodeError as e:
                 print(f"{tabs}Failed to parse binary file")
 
 def main():
     path = sys.argv[1]
 
+    global G
+    global module_set
+
+    module_set = set()
+
     G = nx.DiGraph()
+
+    G.add_node("Node 1", color="blue")
+    G.add_node("Node 2", color="green")
+    G.add_node("Node 3", color="red")
+
+    G.add_edge("Node 1", "Node 2")
+    G.add_edge("Node 2", "Node 3")
+    G.add_edge("Node 3", "Node 1")
     walk_directory(path)
+    pos = nx.spring_layout(G)  # positions for all nodes
+    node_colors = [G.nodes[node]["color"] for node in G.nodes]
+    nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=10)
+    nx.draw_networkx_edges(G, pos, arrows=True)
+    nx.draw_networkx_labels(G, pos, font_size=5, font_family="sans-serif")
+
     plt.axis("off")
     plt.show()
 
